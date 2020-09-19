b652edd832b255cb0de9c3861cc709da4735fdd1
Joris Meys <Joris.Meys@UGent.be>
2020-09-19 16:49:30 +0200

    Split up to namesfunctions as also needed for municipalities.

--- functions/process_shapeinfo.R
+++ functions/process_shapeinfo.R
@@ -1,23 +1,16 @@
 #-----------------------
 # Function to process shapeinfo
-library(stringr)
+# It also contains a bunch of functions 
+source("functions/namesfunctions.R")
+
 process_shapeinfo <- function(x){
-  regs <- recode(x$tx_rgn_descr_nl,
-                 "Vlaams Gewest" = "Flanders",
-                 "Brussels Hoofdstedelijk Gewest" = "Brussels",
-                 "Waals Gewest" = "Wallonia")
-  provs <- case_when(
-    regs == "Flanders" ~ str_remove(x$tx_prov_descr_nl,"Provincie "),
-    regs == "Wallonia" ~ str_remove(x$tx_prov_descr_fr,"Province d[eu] "),
-    regs == "Brussels" ~ "Brussel"
-  )
-  distr <- case_when(
-    regs == "Flanders" ~ str_remove(x$tx_adm_dstr_descr_nl,
-                                    "Arrondissement "),
-    regs == "Wallonia" ~ str_remove(x$tx_adm_dstr_descr_fr,
-                                    "Arrondissement d[eu']\\s?"),
-    regs == "Brussels" ~ str_remove(x$tx_adm_dstr_descr_nl,"Arrondissement ")
-  )
+  regs <- translate_regions(x$tx_rgn_descr_nl)
+  provs <- translate_provinces(x$tx_prov_descr_nl,
+                               x$tx_prov_descr_fr,
+                               regs)
+  distr <- translate_districts(x$tx_adm_dstr_descr_nl,
+                               x$tx_adm_dstr_descr_fr,
+                               regs)
   x %>%
     select(cd_munty_refnis,
            names_munty = tx_munty_descr_nl,
