819cf5e73b8c21476b117b1760609f47b1795f49
Joris Meys <Joris.Meys@UGent.be>
2020-10-04 17:02:30 +0200

    Update animation for positive tests

--- scripts/animationTestVsCases.R
+++ scripts/animationTestVsCases.R
@@ -5,19 +5,20 @@ library(gganimate)
 library(dplyr)
 # For this, you need 
 # source("scripts/downloadData.R")
-regionalsmooth <- readRDS("Processed/regionalsmooth.RDS")
+thefile <- dir("Processed", pattern = "regionalsmooth")
+regionalsmooth <- readRDS(file.path("Processed", thefile))
 # Make plot data
 pdata <- filter(regionalsmooth, REGION == "Belgium",
                 as.Date(DATE) > "2020-05-01",
                 as.Date(DATE) < (Sys.Date() - 4)) %>%
   na.omit() %>%
-  mutate(posrate = round(CASES/TESTS_ALL*100, 1),
+  mutate(posrate = round(TESTS_ALL_POS/TESTS_ALL*100, 1),
          DATE = as.Date(DATE))
 
 # Make raster
 grid <- expand.grid(
  seq(0, max(pdata$TESTS_ALL)*1.05, length.out = 200),
- seq(0, max(pdata$CASES)*1.05, length.out = 200)
+ seq(0, max(pdata$TESTS_ALL_POS)*1.05, length.out = 200)
 )
 
 names(grid) <- c("x","y")
@@ -25,26 +26,33 @@ grid$val <- with(grid, y/x*100)
 grid$val[grid$val > 100] <- 100
 grid <- na.omit(grid)
 
+# Make lines
+refline <- c(0.005, seq(0.01, ceiling(max(pdata$posrate))/100,
+                        by = 0.01))
+
 # Make labels
+maxx <- max(pdata$TESTS_ALL)*1.05
+maxy <- max(pdata$TESTS_ALL_POS)*1.05
 labels <- data.frame(
-  y=c(210,350,650,950, 950, 950),
-  x = c(30000,30000,30000,29000,22000,17000),
-  label = c("0.5%","1%","2%","3%","4%","5%")
+  y=maxx * 0.75 * refline + maxy*0.02,
+  x = rep(maxx*0.75,length(refline)),
+  label = paste0(refline*100,"%")
 )
 
-p <- ggplot(pdata, aes(x=TESTS_ALL, y = CASES)) +
+p <- ggplot(pdata, aes(x=TESTS_ALL, y = TESTS_ALL_POS)) +
   geom_raster(mapping = aes(x=x,y=y,fill=val),
               data = grid) +
   geom_abline(intercept = 0, 
-              slope = c(0.005,0.01, 0.02, 0.03, 0.04, 0.05),
+              slope = refline,
               color = "lightgrey") +
   geom_path(lwd = 2, mapping = aes(color = DATE, group = 1)) +
   geom_point(show.legend = FALSE, size = 3) +
   labs(x = "number of tests",
-       y = "number of cases",
+       y = "number of positive tests",
        fill = "positivity\nrate %") +
   geom_text(mapping = aes(x=x,y=y,label=label),
-            data = labels) +
+            data = labels,
+            hjust = "right") +
   scale_y_continuous(limits = c(0,NA), expand = c(0,0.5) ) +
   scale_x_continuous(limits = c(0,NA), expand = c(0,0.5) ) +
   theme_classic() +
@@ -55,7 +63,8 @@ p <- ggplot(pdata, aes(x=TESTS_ALL, y = CASES)) +
                        values = c(0,0.3,0.4,0.5,0.6,0.7,
                                   0.8,0.9,1),
                        trans = "log10") +
-  labs(caption = "Data from https://epistat.wiv-isp.be/covid/")
+  labs(caption = paste("Data from https://epistat.wiv-isp.be/covid/,",
+                       "downloaded on",Sys.Date()))
 
 anim <- p + transition_reveal(DATE) +
   labs(title = "Date: {pdata$DATE[round(frame)]} - positivity rate: {sprintf('%1.1f', pdata$posrate[round(frame)]) } % - data for Belgium")
@@ -67,8 +76,9 @@ anim <- p + transition_reveal(DATE) +
 anim_save("testVsCases.gif",
           anim,
           path = "Data",
-          nframes = nrow(pdata) + 20,
-          fps = 5, end_pause = 20,
+          nframes = nrow(pdata) + 100,
+          fps = 20, end_pause = 100,
           width = 900,
           height = 900,
           res = 150)
+
