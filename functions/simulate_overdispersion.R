b40a64eb7602d3fb4901025a7668f609081dbc72
Joris Meys <Joris.Meys@UGent.be>
2020-10-15 15:36:25 +0200

    Add parameter info simulation overdispersion

--- functions/simulate_overdispersion.R
+++ functions/simulate_overdispersion.R
@@ -2,7 +2,12 @@
 # distribution to take overdispersion into account.
 #---------------------------------------------------------
 
-# Simulate total number of infections after n steps
+#----------------------------------------------------------
+#' Simulate total number of infections after n steps
+#' @param nsteps the number of steps in the infection chain
+#' @param nstart the number of infected patients
+#' @param k the overdispersion parameter
+#' @param R the reproductive number (seen as mean of the neg binom dist)
 simul_cases <- function(nsteps = 5, nstart = 1,
                         k = 0.1, R = 1.3){
   n <- nstart
@@ -14,7 +19,10 @@ simul_cases <- function(nsteps = 5, nstart = 1,
   n
 }
 
-# Make distribution for a specific simulation
+#---------------------------------------------
+#' Make distribution for a specific simulation
+#' @param nsimul the number of simulations
+#' @inheritParams simul_cases
 dist_cases <- function(nsimul = 1000,
                        nsteps = 5, nstart = 1,
                        k = 0.1, R = 1.3){
@@ -25,7 +33,11 @@ dist_cases <- function(nsimul = 1000,
                         R = R))
 }
 
-# Make probability distribution of dying epidemic
+#--------------------------------------------------
+#' Make probability distribution of dying epidemic
+#' @param nstart a sequence of values for nstart (see simul_cases)
+#' @inheritParams dist_cases
+
 simul_prob <- function(nstart = seq.int(50),
                        nsimul = 1000,
                        nsteps = 5,
@@ -44,10 +56,14 @@ simul_prob <- function(nstart = seq.int(50),
          p = p)
 }
 
-# Simulate an infection chain
-# Function to simulate an infection chain
-simul_chain <- function(k=0.1, R = 1.3, ninfected = 10, nsteps = 5){
+#-------------------------------
+#' Simulate an infection chain
+#' @inheritParams simul_cases
+#' @return a tidygraph with a random infection chaing
+
+simul_chain <- function(k=0.1, R = 1.3, nstart = 10, nsteps = 5){
   # Make the start cases
+  ninfected <- nstart
   start <- paste0("r1case0-",seq.int(ninfected))
   
   # initialize the vectors to store results
@@ -95,4 +111,35 @@ simul_chain <- function(k=0.1, R = 1.3, ninfected = 10, nsteps = 5){
             edges = edge,
             directed = TRUE)
   
+}
+
+#-------------------------------------------
+#' Plot an infection chain
+#' @param x a tidygraph object, coming from simul_chain
+#' @param title title for the graph
+#' @param maxcol the maximum number of infections getting their own
+#' color. If maxcol is 2, you get the categories 0,1,2,>2. 
+plot_chain <- function(x, title = "Spread",maxcol = 10){
+  x <- x %>% activate(nodes) %>%
+    mutate(ninfected = ifelse(level == 0,0,ninfected),
+           dinfected = cut(ninfected,
+                           breaks = c((-1):10,+Inf),
+                           labels = c(as.character(0:10), ">10")))
+  p1 <- ggraph(x, layout = "partition",
+               direction = "out") +
+    geom_edge_link() +
+    geom_node_point(aes(color = dinfected,
+                        size = sqrt(ninfected + 1) )) +
+    theme_void() +
+    scale_color_viridis_d(direction = -1, option = "B", end = 0.8, limits = c(as.character(0:10), ">10")) +
+    guides(size = FALSE) +
+    labs(color = "number\ninfected",
+         title = title) 
+  
+  vert <- as_tibble(x) 
+  p2 <- ggplot(vert, aes(y=level)) +
+    geom_bar() +theme_minimal() +
+    labs(y = "") +
+    theme(axis.text.y = element_blank())
+  p1 + p2 + plot_layout(widths = c(5,1)) 
 }
