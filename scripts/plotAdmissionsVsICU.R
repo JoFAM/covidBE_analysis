34bb694884cd0082d66415dae9eb65545d8935a9
Joris Meys <Joris.Meys@UGent.be>
2021-04-14 12:33:23 +0200

    Update ICU plot for april 12, 2021

--- scripts/plotAdmissionsVsICU.R
+++ scripts/plotAdmissionsVsICU.R
@@ -40,7 +40,7 @@ ggplot(slice, aes(x = DATE, y = smoothICU )) +
   theme_minimal() +
   labs(y = "Number of patients",
        fill = "Daily new\nhospitalisations",
-       subtitle = "Data downloaded from https://epistat.wiv-isp.be/Covid/ on 2020-03-05",
+       subtitle = paste("Data downloaded from https://epistat.wiv-isp.be/Covid/ on", Sys.Date()) ,
        caption = "@JorisMeys") + 
   ggtitle("Patients in ICU - Belgium") +
   scale_fill_identity(
@@ -116,6 +116,22 @@ ggplot(slice, aes(x = DATE, y = smoothICU )) +
            label = "Peak in ICU total 10 days after\npeak in new hospitalisations",
            color = "black") +
   scale_x_date(date_breaks = "2 month",
-               date_labels = "%b %d")
+               date_labels = "%b %d") +
+  geom_segment(x = as.Date("2020-03-01"),
+               xend = as.Date("2021-04-12"),
+               y = 269,
+               yend = 269, col = "red", lty = 2) +
+  geom_segment(x = as.Date("2021-04-01"),
+               xend = as.Date("2021-04-01"),
+               y = 0,
+               yend = 917, col = "red", lty = 2) +
+  geom_segment(x = as.Date("2020-03-01"),
+               xend = as.Date("2021-04-12"),
+               y = 917,
+               yend = 917, col = "blue", lty = 2) +
+  geom_segment(x = as.Date("2021-04-12"),
+               xend = as.Date("2021-04-12"),
+               y = 0,
+               yend = 917, col = "blue", lty = 2)
 
 
